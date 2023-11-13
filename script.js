14b485a4d9f132e5ca2e9fbe73e687b51fc67401
Lorran Franklin <lorranfranklin.dev@gmail.com>
2023-11-13 09:24:02 -0300

    mudando img do perfil

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function toggleMode() {
   const html = document.documentElement
-
   html.classList.toggle("light")
+
+  // pegar tag img
+  const img = document.querySelector("#profile img")
+  
+  // substituir img
+  if (html.classList.contains("light")) {
+    // se tiver light mode adicionar a imagem amanhecer
+    img.setAttribute("src", "./assets/4.png")
+    img.setAttribute("alt", "Imagem do amanhecer")
+  } else {
+    // se tiver sem light mode, adicionar imagem da noite
+    img.setAttribute("src", "./assets/1.png")
+    img.setAttribute("alt", "Imagem da noite")
+  }
 }
